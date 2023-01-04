2bdd4b684cbb66757aa11682b468a0ed6c3e4e3f
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2023-01-04 11:44:10 +0100

    Update python.py

--- python.py
+++ python.py
@@ -4,7 +4,7 @@ import numpy as np
 import requests
 
 
-response = requests.get("http://localhost:8000/API/V1/vehicles")
+response = requests.get("https://bussie.vdotvo9a4e2a6.eu-central-1.cs.amazonlightsail.com/API/V1/get_all_vehicle_information")
 response = response.json()
 df = pd.DataFrame.from_dict(response)
 
